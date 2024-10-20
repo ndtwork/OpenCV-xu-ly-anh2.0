d1c15b56a454d1f5b7f51a608c6c4d6429bc49d4
Nguyễn Đức Tú <nguyenductu.work@gmail.com>
2024-10-21 00:25:41 +0700

    1
    como

--- testing file/test4.py
+++ testing file/test4.py
@@ -25,6 +25,7 @@ X2 = np.array([
     [18, 19, 18, 17, 3, 1, 1, 3]
 ])
 
+
 # Vẽ histogram của X1 và X2
 plt.figure(figsize=(12, 6))
 
@@ -53,33 +54,28 @@ plt.figure(figsize=(12, 6))
 
 plt.subplot(1, 2, 1)
 plt.imshow(X2, cmap='gray')
-plt.title('Ảnh X2 gốc')
+plt.title('Ảnh {X2} gốc')
 
 plt.subplot(1, 2, 2)
 plt.imshow(X2_equalized, cmap='gray')
-plt.title('Ảnh X2 sau khi cân bằng Histogram')
+plt.title('Ảnh {X2} sau khi cân bằng Histogram')
 
 plt.tight_layout()
 plt.show()
 
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.hist(X2.ravel(), bins=256, range=(0, 255), color='gray')
+plt.title('Histogram của X2')
+plt.xlabel('Mức xám')
+plt.ylabel('Số lượng pixel')
+
 # Histogram sau khi cân bằng
-plt.figure(figsize=(6, 6))
+plt.subplot(1, 2, 2)
 plt.hist(X2_equalized.ravel(), bins=256, range=(0, 1), color='gray')
 plt.title('Histogram sau khi cân bằng của X2')
 plt.xlabel('Mức xám (Sau khi cân bằng)')
 plt.ylabel('Số lượng pixel')
-plt.show()
-
-
-from scipy.ndimage import convolve
-
-# Mask lọc trung bình
-average_filter = np.ones((3, 3)) / 9
-
-# Lọc trung bình
-X2_average_filtered = convolve(X2, average_filter)
-
-plt.imshow(X2_average_filtered, cmap='gray')
-plt.title('Lọc trung bình trên X2')
 plt.show()
 
